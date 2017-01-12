b9bd5c3e9712bb0df9fc0ac47cb015b80ac7a5b3
Rik Goldman <rikgoldman@gmail.com>
2017-01-12 13:00:43 -0500

    add python for ex3

--- ex3/quote.py
+++ ex3/quote.py
@@ -2,6 +2,12 @@
 # ism 1
 # quote and author (ex3)
 
+# This asks for a quote. It calls the answer "quote"
 quote=raw_input("What is the quote? ")  
+
+# This asks who said the quote. It calls the answer "author"
 author=raw_input("Who said it? ") 
+
+# prints a sentence with the quote and author.
 print author + " says," + " \"" + quote + "\""
+
